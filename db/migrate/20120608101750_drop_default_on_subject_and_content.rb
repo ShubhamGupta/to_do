9c32c2daa05bc7ddf8cbf87ca5819ea6e9919bfd
Shubham Gupta <shubham@Gupta.(none)>
2012-06-08 16:09:44 +0530

    migration_changes

--- db/migrate/20120608101750_drop_default_on_subject_and_content.rb
+++ db/migrate/20120608101750_drop_default_on_subject_and_content.rb
@@ -1,12 +1,15 @@
 class DropDefaultOnSubjectAndContent < ActiveRecord::Migration
   def up
-		change_table :to_do_itemsc do |t|
-		  t.remove :subject, :content
-		  t.string :subject
-		  t.string :content
+		change_table :to_do_items do |t|
+		  t.change :subject, :string
+		  t.change :content, :string
 		end
   end
 
   def down
+  change_table :to_do_items do |t|
+	  t.change :subject, :string, :default => ''
+	  t.change :content, :string, :default => ''
+	  end
   end
 end
